c7bdac6e5203d6b3c0274622dce2057bd0350895
Max Eichhorn <m.3ichhorn@gmail.com>
2020-10-18 19:15:59 +0200

    Second Commit Checklist

--- src/components/checklist.vue
+++ src/components/checklist.vue
@@ -3,10 +3,17 @@
       <h2>Compliance Checklist</h2>
        <div class="headerBasic">
         
-            <div v-for="(item, index) in projectBasics" :key="index" class="headerElement">{{index}}: {{item}}</div>
+            <div v-for="(item, index) in projectBasics" :key="index" class="headerElement"> <span>{{index}}: </span>{{item}}</div>
         
     </div>
-    <div>{{projectAdvanced}}</div>
+
+    <div class="headerBasic">
+        
+            <div v-for="(item, index) in projectAdvanced" :key="index" class="headerElement"> <span>{{item.name}}:</span> {{item.value}}</div>
+        
+    </div>
+
+    
    <!-- <div>{{ ruleSet }}}</div> -->
   </div>
 </template>
@@ -34,11 +41,19 @@ export default {
     margin: auto;
     flex-wrap: wrap;
     flex-direction:row;
+    border: 2px black solid;
+    margin-bottom: 10px;
 
 }
 .headerElement{
     width: 49.5%;
     border: 2px hsl(200, 30%, 30%) solid;
     margin: 1px 1px 1px 1px;
+    background-color:hsl(200, 30%, 30%) ;
+    color:white;
+    
+}
+span{
+    color: lightgrey
 }
 </style>
